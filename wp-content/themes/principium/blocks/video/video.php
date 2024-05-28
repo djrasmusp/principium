ba61fe17415954698b2b64aade932d6468068f17
Rasmus P <djrasmusp@users.noreply.github.com>
2024-05-28 20:39:10 +0200

    updated video design

--- wp-content/themes/principium/blocks/video/video.php
+++ wp-content/themes/principium/blocks/video/video.php
@@ -34,12 +34,11 @@ if(!class_exists('BLOCK_VIDEO')) {
             <section class="<?= $this->get_class_name() ?> mt-8" id="<?= $this->get_id() ?>">
                 <div class="px-6 md:px-16 max-w-5xl">
                     <?php if($this->get_title()) : ?>
-                    <h2 class="text-base mb-4 md:text-lg font-header uppercase tracking-widest"><?= $this->get_title() ?></h2>
+                    <h2 class="text-base mb-4 text-normal text-page-text normal-case font-normal"><?= $this->get_title() ?></h2>
                     <?php endif; ?>
-                    <a href="<?= $this->get_video()['url'] ?>#t=0.0001" data-autoplay data-fslightbox class="aspect-square w-full umami--click--video-<?= sanitize_title($this
-                    ->get_video['title']) ?>">
+                    <a href="<?= $this->get_video()['url'] ?>#t=0.0001" data-autoplay data-fslightbox class="aspect-square w-full umami--click--video-<?= sanitize_title($this->get_video()['title']) ?>">
                         <div
-                                class="relative aspect-video w-full overflow-hidden bg-header-bg <?= $this->is_preview ? "" : "group" ?>">
+                                class="relative aspect-video w-full overflow-hidden bg-darkgrey <?= $this->is_preview ? "" : "group" ?>">
                             <video src="<?= $this->get_video()['url'] ?>#t=0.000001" class="absolute inset-0 object-cover w-full h-full z-0 transition-all duration-450 opacity-100 group-hover:scale-110 group-hover:opacity-30"></video>
                             <div class="relative inset-0 flex h-full w-full items-center justify-center">
                                 <button type="button"
